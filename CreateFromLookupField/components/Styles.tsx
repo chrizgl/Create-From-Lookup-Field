d5b4db84d770f2bc69f963c70d34eba11c0c4929
Christian Gierl <c.gierl@outlook.com>
2023-08-28 18:58:36 +0200

    to sync with draegerwerk

---
+++ CreateFromLookupField/components/Styles.tsx
@@ -0,0 +1,50 @@
+import { makeStyles } from '@fluentui/react-components';
+
+export const useStyles = makeStyles({
+    stack: {
+        // must be merged with stackHorizontal or stackVertical
+        display: 'flex',
+        flexWrap: 'nowrap',
+        width: '100%',
+        height: 'fit-content',
+        boxSizing: 'border-box',
+        '> *': {
+            textOverflow: 'ellipsis',
+        },
+    },
+    stackHorizontal: {
+        // overrides for horizontal stack
+        flexDirection: 'row',
+        marginLeft: '0px',
+        '> :not(:last-child)': {
+            marginRight: '1px',
+        },
+    },
+    stackVertical: {
+        // overrides for vertical stack
+        flexDirection: 'column',
+        marginLeft: '5px',
+        '> :not(:first-child)': {
+            marginTop: '10px',
+        },
+    },
+    stackitem: {
+        height: 'fit-content',
+        width: '100%',
+        alignSelf: 'right',
+        flexShrink: 1,
+    },
+    tooltip: {
+        paddingLeft: '0px',
+        paddingRight: '0px',
+        paddingTop: '0px',
+        paddingBottom: '0px',
+    },
+    overflow: {
+        color: 'forestgreen',
+        scale: 1.5,
+    },
+    icon: {
+        scale: 1.3,
+    },
+});
