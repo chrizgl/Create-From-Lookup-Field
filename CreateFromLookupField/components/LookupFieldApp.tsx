c9987058911119aa1040606ee13dff0ef3affedf
Christian Gierl <c.gierl@outlook.com>
2023-08-29 19:22:55 +0200

    design changed fundamental

--- CreateFromLookupField/components/LookupFieldApp.tsx
+++ CreateFromLookupField/components/LookupFieldApp.tsx
@@ -1,50 +1,145 @@
 import * as React from 'react';
+import { Checkmark32Regular, Checkmark32Filled, Copy32Regular, CheckboxChecked24Regular } from '@fluentui/react-icons';
 import {
+    makeStyles,
     mergeClasses,
     Button,
     FluentProvider,
     webLightTheme,
-    Combobox,
-    ComboboxProps,
-    ComboboxState,
     Input,
     InputProps,
-    InputOnChangeData,
     useId,
+    shorthands,
+    tokens,
+    ColorPaletteTokens,
     Overflow,
 } from '@fluentui/react-components';
-import { useStyles } from './Styles';
 import { useState } from 'react';
-import { on } from 'events';
 
-export interface ILookupFieldProps {
-    lookupValue: ComponentFramework.LookupValue;
-    performLookupObjects: (lookupValue: ComponentFramework.LookupValue) => void;
-    onInputChange: () => void;
+export interface ICreateFromLookupProps {
+    lookupField: ComponentFramework.PropertyTypes.LookupProperty;
+    utils: ComponentFramework.Utility;
+    isDisabled: boolean;
+    currentValue: string
 }
 
-const LookupFieldApp = (props: ILookupFieldProps): JSX.Element => {
+export interface ICreateFromLookupState {
+    currentValue: string;
+    overlayHidden: boolean;
+    iconBackground: string;
+}
+
+interface IClipboardClassObject {
+    clipboardIcon: string;
+    textbox: string;
+    iconWrapper: string;
+    textboxOverlay: string;
+}
+
+const useStyles = makeStyles({
+    stack: {
+        // must be merged with stackHorizontal or stackVertical
+        display: 'flex',
+        flexWrap: 'nowrap',
+        width: '100%',
+        height: 'fit-content',
+        boxSizing: 'border-box',
+        '> *': {
+            textOverflow: 'ellipsis',
+        },
+    },
+    stackHorizontal: {
+        // overrides for horizontal stack
+        flexDirection: 'row',
+        marginLeft: '0px',
+        '> :not(:last-child)': {
+            marginRight: '1px',
+        },
+    },
+    stackVertical: {
+        // overrides for vertical stack
+        flexDirection: 'column',
+        marginLeft: '5px',
+        '> :not(:first-child)': {
+            marginTop: '10px',
+        },
+    },
+    stackitem: {
+        height: 'fit-content',
+        width: '100%',
+        alignSelf: 'right',
+        flexShrink: 1,
+    },
+    stackitemSliderVertical: {
+        alignSelf: 'left',
+        marginLeft: '10px',
+        flexShrink: 1,
+    },
+    stackitemBadgeVertical: {
+        alignSelf: 'left',
+        marginLeft: '5px',
+        flexShrink: 1,
+    },
+    tooltip: {
+        paddingLeft: '0px',
+        paddingRight: '0px',
+        paddingTop: '0px',
+        paddingBottom: '0px',
+    },
+    overflow: {
+        color: 'forestgreen',
+        scale: 1.5,
+    },
+    icon: {
+        scale: 1.3,
+    },
+    input: {
+        ...shorthands.border('0px', 'solid', tokens.colorNeutralStroke1),
+        backgroundColor: '#f5f5f5',
+    },
+});
+
+const CreateFromLookupApp = (props: ICreateFromLookupProps): JSX.Element => {
     const classes = useStyles();
     const stackClasses = mergeClasses(classes.stack, classes.stackHorizontal);
-    const id = useId();
-    const [inputValue, setInputValue] = useState(props.lookupValue.name ?? '');
-    const onComboboxChange: ComboboxProps['onChange'] = () => {};
+    const overflowClass = mergeClasses(classes.overflow, classes.stackitem);
+    const inputClass = mergeClasses(classes.input, classes.stackitem);
+    const iconClass = mergeClasses(classes.icon, classes.stackitem);
 
-    const onButtonClick = (props: ILookupFieldProps) => {
-        return () => {
-            props.performLookupObjects(props.lookupValue);
-        };
+    const id = useId();
+    const [inputValue, setInputValue] = useState(props.currentValue);
+    const [state, setState] = useState<ICreateFromLookupState>({
+        currentValue: '',
+        overlayHidden: true,
+        iconBackground: 'transparent',
+    });
+    const onKeyFunction: InputProps['onKeyDown'] = (key) => {
+        console.log(inputValue)
+    };
+
+    const iconOnClick = () => {
+        setState((state) => ({ ...state, overlayHidden: false, iconBackground: 'lightgreen' }));
+        setTimeout(() => {
+            setState((state) => ({ ...state, overlayHidden: true, iconBackground: 'transparent' }));
+        }, 1000);
+    };
+
+    const showIcon = () => {
+        if (state.overlayHidden) {
+            return <Copy32Regular className={iconClass}></Copy32Regular>;
+        } else {
+            return <Checkmark32Filled className={overflowClass}></Checkmark32Filled>;
+        }
     };
 
     return (
         <FluentProvider theme={webLightTheme}>
             <div className={stackClasses}>
-                <Input></Input>
-                <Combobox onChange={onComboboxChange}></Combobox>
-                <Button onClick={onButtonClick(props)} />
+                <Input id={id} readOnly={props.isDisabled} className={inputClass} value={inputValue} onKeyDown={onKeyFunction} onChange={e => e.target.value} />
+                <Button className={classes.stackitem} icon={showIcon()} onClick={iconOnClick}></Button>
             </div>
         </FluentProvider>
     );
 };
 
-export default LookupFieldApp;
+export default CreateFromLookupApp;
--- CreateFromLookupField/index.ts
+++ CreateFromLookupField/index.ts
@@ -1,140 +1,54 @@
 import { IInputs, IOutputs } from './generated/ManifestTypes';
-import LookupFieldApp, { ILookupFieldProps } from './components/LookupFieldApp';
+import CreateFromLookupApp, { ICreateFromLookupProps } from './components/LookupFieldApp';
 import { createElement } from 'react';
 import { createRoot, Root } from 'react-dom/client';
 
 export class CreateFromLookupField implements ComponentFramework.StandardControl<IInputs, IOutputs> {
-    private _root: Root;
-
-    // Reference to ComponentFramework Context object
-    private _context: ComponentFramework.Context<IInputs>;
-
-    // PCF framework delegate which will be assigned to this object which would be called whenever any update happens
     private _notifyOutputChanged: () => void;
+    private _root: Root;
+    private _lookupField: any[] = [];
+    private _isReadOnly: boolean;
+    private _defaultEntityType : string;
+	private _viewId : string;
+	private _entityTypes : string[];
+	private _viewIds : string[];
+    private _currentValue: string
 
-    // Values to be filled based on control context, passed as arguments to lookupObjects API
-    private _entityType = '';
-    private _defaultViewId = '';
-
-    // Used to store necessary data for a single lookup entity selected during runtime
-    private _selectedItem: ComponentFramework.LookupValue;
-
-    // Used to track which lookup property is being updated
-    private _updateSelected = false;
-
-    private _lookupValues: ComponentFramework.LookupValue[];
-
-    /**
-     * Empty constructor.
-     */
     constructor() {}
 
-    /**
-     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-     * Data-set values are not initialized here, use updateView.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-     * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-     */
     public init(
         context: ComponentFramework.Context<IInputs>,
         notifyOutputChanged: () => void,
         state: ComponentFramework.Dictionary,
         container: HTMLDivElement,
-    ): void {
-        this._entityType = context.parameters.lookupfield.getTargetEntityType();
-        this._defaultViewId = context.parameters.lookupfield.getViewId();
+    ) {
         this._root = createRoot(container!);
         this._notifyOutputChanged = notifyOutputChanged;
-        this.performLookupObjects.bind(this, this._entityType, this._defaultViewId, (value, update = true) => {
-            this._selectedItem = value;
-            this._updateSelected = update;
-        });
     }
 
-    /**
-     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-     */
     public updateView(context: ComponentFramework.Context<IInputs>): void {
-        const lookupValue: ComponentFramework.LookupValue = context.parameters.lookupfield.raw[0];
-        const props: ILookupFieldProps = {
-            lookupValue: lookupValue,
-            performLookupObjects: this.performLookupObjects.bind(this, this._entityType, this._defaultViewId, (value, update = true) => {
-                this._selectedItem = value;
-                this._updateSelected = update;
-            }),
-            onInputChange: () => {
-                this._notifyOutputChanged();
-            },
+        const lookupField = context.parameters.lookupField;
+        const isDisabled = context.mode.isControlDisabled;
+
+        const props: ICreateFromLookupProps = {
+            lookupField: lookupField,
+            utils: context.utils,
+            isDisabled: false,
+            currentValue: this._currentValue
         };
-        this._context = context;
-        this._root.render(createElement(LookupFieldApp, props));
+        this._root.render(createElement(CreateFromLookupApp, props));
     }
 
-    /**
-     * It is called by the framework prior to a control receiving new data.
-     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
-     */
     public getOutputs(): IOutputs {
-        // Send the updated selected lookup item back to the ComponentFramework, based on the currently selected item
         return {
-            lookupfield: [this._selectedItem],
         };
     }
 
-    /**
-     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-     * i.e. cancelling any pending remote calls, removing listeners, etc.
-     */
     public destroy(): void {
         this._root.unmount();
     }
-    /*******************/
-    /*PRIVATE FUNCTIONS*/
-    /*******************/
 
-    private performLookupObjects(
-        entityType: string,
-        viewId: string,
-        setSelected: (value: ComponentFramework.LookupValue, update?: boolean) => void,
-    ): void {
-        // Used cached values from lookup parameter to set options for lookupObjects API
-        const lookupOptions = {
-            defaultEntityType: entityType,
-            defaultViewId: viewId,
-            allowMultiSelect: false,
-            entityTypes: [entityType],
-            viewIds: [viewId],
-        };
-
-        this._context.utils.lookupObjects(lookupOptions).then(
-            (success) => {
-                if (success && success.length > 0) {
-                    // Cache the necessary information for the newly selected entity lookup
-                    const selectedReference = success[0];
-                    const selectedLookupValue: ComponentFramework.LookupValue = {
-                        id: selectedReference.id,
-                        name: selectedReference.name,
-                        entityType: selectedReference.entityType,
-                    };
-
-                    // Update the primary or secondary lookup property
-                    setSelected(selectedLookupValue);
-
-                    // Trigger a control update
-                    this._notifyOutputChanged();
-                } else {
-                    setSelected({} as ComponentFramework.LookupValue);
-                }
-            },
-            (error) => {
-                console.log(error);
-            },
-        );
-    }
-    private notifyChange() {
+    private onChange = () => {
         this._notifyOutputChanged();
-    }
+    };
 }
